ff8501b15268a1904975c2c478371ef1306b05b5
Timo Reunanen <parker@friikz.net>
2015-03-24 19:19:52 +0200

    Now able to run run script anywhere

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
-export GOPATH=`pwd`/_vendor:`pwd`
+#! /bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+export GOPATH=$DIR/_vendor:$DIR
 
 go get all
-docker build -t remotego .
+docker build -t remotego $DIR
 docker run --rm -ti --publish 3000:3000 --name test remotego
