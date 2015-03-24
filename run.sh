cd2c75fb34f635aa4c07dad6fcb3168e044c3a68
Timo Reunanen <parker@friikz.net>
2015-03-25 01:07:11 +0200

    Now able to use nodemon for auto reloading on file changes

--- run.sh
+++ run.sh
@@ -10,4 +10,18 @@ docker build -t httpserver $DIR
 ## Remove unused images
 docker rmi $(docker images -f 'dangling=true' -q) &> /dev/null &
 
-docker run --rm -ti --publish 3000:3000 --name test httpserver
+id=$(docker run -d --publish 3000:3000 httpserver)
+
+function cleanup() {
+	echo "Cleanup..."
+
+	docker kill $id &> /dev/null
+	docker wait $id &> /dev/null
+	docker rm $id &> /dev/null
+	exit 0
+}
+
+trap cleanup EXIT
+
+docker logs $id
+docker attach --sig-proxy=false $id 
---
+++ watch.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+nodemon -x ./run.sh -w src -e go
