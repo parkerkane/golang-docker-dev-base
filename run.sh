d1be39cfb8fe2fb40155e284d849853e0c0cd728
Timo Reunanen <parker@friikz.net>
2015-03-24 19:44:11 +0200

    Auto clear unused images

--- run.sh
+++ run.sh
@@ -5,5 +5,9 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export GOPATH=$DIR/_vendor:$DIR
 
 go get all
-docker build -t remotego $DIR
-docker run --rm -ti --publish 3000:3000 --name test remotego
+docker build -t httpserver $DIR
+
+## Remove unused images
+docker rmi $(docker images -f 'dangling=true' -q) &> /dev/null &
+
+docker run --rm -ti --publish 3000:3000 --name test httpserver
