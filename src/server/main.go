29858335a140f7b5b6e07e12c3628aa93d214695
Timo Reunanen <parker@friikz.net>
2015-03-25 02:53:50 +0200

    Added few things in example server

--- src/server/main.go
+++ src/server/main.go
@@ -1,11 +1,37 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"github.com/martini-contrib/render"
+	"net/http"
 
+	"github.com/go-martini/martini"
+	"github.com/jamieomatthews/validation"
+	"github.com/martini-contrib/binding"
+)
+
+type BlogPost struct {
+	Title   string `json:"title" binding:"required"`
+	Content string `json:"content"`
+	Status  int    `json:"status"`
+}
+
+func (bp BlogPost) Validate(errors binding.Errors, req *http.Request) binding.Errors {
+
+	v := validation.NewValidation(&errors, bp)
+
+	v.Validate(&bp.Title).Key("title").MinLength(4)
+
+	return *v.Errors.(*binding.Errors)
+}
 func main() {
 	m := martini.Classic()
-	m.Get("/", func() string {
-		return "Hello world!"
+
+	m.Use(render.Renderer())
+
+	m.Post("/blog", binding.Json(BlogPost{}), binding.ErrorHandler, func(blogpost BlogPost, r render.Render) {
+		blogpost.Status = 42
+		r.JSON(200, blogpost)
 	})
+
 	m.Run()
 }
